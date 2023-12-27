dfed79884c4e89ee4e5b32af3c15f0e985b85303
Vedant-2309 <Vedant-2309@users.noreply.github.com>
2023-12-27 20:59:38 +0000

    Test

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ import Pill from "./components/pill.js";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5 bg-white w-full h-full">
-      <Image
+      {/* <Image
         src="/icons/pfp.png"
         alt="rounded photo of a cube"
         height={150}
         width={150}
         className="rounded-full"
-      />
+      /> */}
       <Pill src="/icons/pfp2.jpg" alt="image 1" text="Random photo 2" />
       <Pill src="/icons/pfp3.jpg" alt="image 2" text="Random photo 3" />
       <Pill src="/icons/pfp4.jpg" alt="image 3" text="Random photo 4" />
