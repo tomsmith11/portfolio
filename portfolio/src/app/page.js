0ba0fe4e8cc4fe5d780156bad2b108fb7b98a776
Tom Smith <tom@craftscale.uk>
2023-12-27 18:55:39 +0000

    Added main photo of square

--- portfolio/src/app/page.js
+++ portfolio/src/app/page.js
@@ -1,9 +1,17 @@
+import Image from "next/image";
+
 import Pill from "./components/pill.js";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-5 bg-white w-full h-full">
-      <Pill src="/icons/pfp.png" alt="photo of a cube" text="Random photo 1" />
+      <Image
+        src="/icons/pfp.png"
+        alt="rounded photo of a cube"
+        height={150}
+        width={150}
+        className="rounded-full"
+      />
       <Pill src="/icons/pfp2.jpg" alt="image 1" text="Random photo 2" />
       <Pill src="/icons/pfp3.jpg" alt="image 2" text="Random photo 3" />
       <Pill src="/icons/pfp4.jpg" alt="image 3" text="Random photo 4" />
